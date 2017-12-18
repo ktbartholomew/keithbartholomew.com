0262313ce4971c4d15273a9fb24c4427f78e7dc6
Keith Bartholomew <keith.bartholomew@rackspace.com>
2017-12-18 14:30:51 -0600

    fetch an auth token before using GH API

--- src/js/bin/gh.js
+++ src/js/bin/gh.js
@@ -1,33 +1,67 @@
 var Executable = require('../lib/files/executable');
 
-module.exports = new Executable({
-  name: 'gh',
-  main: function () {
-    var usage = () => {
-      this.stdout.write('Usage: gh get repos\r\n')
-      this.exit(1);    
+var AccessToken = {
+  Get: () => {
+    return localStorage.getItem('gh_token');
+  },
+  Set: (value) => {
+    return localStorage.setItem('gh_token', value);
+  },
+  Renew: (callback) => {
+    var messageHandler = e => {
+      if (e.origin !== window.location.origin) {
+        return;
+      }
+
+      AccessToken.Set(e.data);
+      window.removeEventListener('message', messageHandler);
+      e.source.close();
+      callback();
     };
 
+    window.addEventListener('message', messageHandler);
+    window.open('/usr/local/bin/gh/oauth/authorize');
+  }
+}
+
+module.exports = new Executable({
+  name: 'gh',
+  main: function() {
+    var usage = () => {
+      this.stdout.write('Usage: gh get repos\r\n');
+      this.exit(1);
+    };
+
+    var getRepos = () => {
+      var authHeaders = new Headers({
+        Authorization: 'token ' + AccessToken.Get()
+      });
+
+      fetch('/usr/local/bin/gh/proxy/users/ktbartholomew/repos', {headers: authHeaders})
+        .then(res => {
+          return res.json();
+        })
+        .then(repos => {
+          repos.forEach(repo => {
+            this.stdout.write(repo.full_name + '\r\n');
+          });
+
+          this.exit(0);
+        })
+        .catch(err => {
+          console.log(err);
+          this.exit(1);
+        });
+    };
 
     if (arguments[1] !== 'get' && arguments[2] !== 'repos') {
       return usage();
     }
 
-    fetch('https://api.github.com/users/ktbartholomew/repos')
-    .then((res) => {
-      return res.json();
-    })
-    .then((repos) => {
-      repos.forEach((repo) => {
-        this.stdout.write(repo.full_name + '\r\n');
-      });
+    if (AccessToken.Get() === null || AccessToken.Get() === '') {
+      return AccessToken.Renew(getRepos);
+    }
 
-      this.exit(0);
-    })
-    .catch((err) => {
-      console.log(err);
-      this.exit(1);
-    })
+    getRepos();
   }
 });
-
