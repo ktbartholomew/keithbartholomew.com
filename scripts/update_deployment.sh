dd747d0ff90b7399bed1d5ee981fbdd851499add
Keith Bartholomew <keith.bartholomew@rackspace.com>
2018-10-31 17:58:14 -0500

    actually do the deployment

--- scripts/update_deployment.sh
+++ scripts/update_deployment.sh
@@ -6,4 +6,14 @@ echo "Unpacking kubeconfig from environment"
 mkdir -p ~/.kube
 echo -n $CI_DEPLOYER_KUBECONFIG | base64 -d > ~/.kube/config
 
-kubectl get deployment keithbartholomew -o yaml -n keithbartholomew
+function rollout() {
+    kubectl set image deployment/keithbartholomew keithbartholomew=quay.io/ktbartholomew/keithbartholomew.com:${CIRCLE_SHA1} -n keithbartholomew --record
+    kubectl rollout status deployment/keithbartholomew -n keithbartholomew
+}
+
+function rollback() {
+    kubectl rollout undo deployment/keithbartholomew -n keithbartholomew
+    exit 1
+}
+
+rollout || rollback
