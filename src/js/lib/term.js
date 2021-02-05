5b3f228fb05eb66c546c939e972e785124384243
Keith Bartholomew <kbartholomew@godaddy.com>
2021-02-04 21:17:36 -0600

    upgrade to xtermjs 4

--- src/js/lib/term.js
+++ src/js/lib/term.js
@@ -1,6 +1,20 @@
-import Terminal from 'xterm';
+import { Terminal } from 'xterm';
+import chalk from 'chalk';
+chalk.level = 2;
 
 export default new Terminal({
   cursorBlink: true,
-  visualBell: true
+  visualBell: true,
+  theme: {
+    background: '#002b36',
+    foreground: '#eee8d5',
+    yellow: '#b58900',
+    orange: '#cb4b16',
+    red: '#dc322f',
+    magenta: '#d33682',
+    violet: '#6c71c4',
+    blue: '#268bd2',
+    cyan: '#2aa198',
+    green: '#859900',
+  }
 });
