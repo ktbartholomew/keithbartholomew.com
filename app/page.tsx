0cd016bc5242e260f80724f1af45fb755392bd76
Keith Bartholomew <keith.bartholomew@gmail.com>
2024-04-27 22:22:56 -0500

    add recent blog posts to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
+import Link from "next/link";
+import { listPosts } from "./blog/posts";
+import { MDXRemote } from "next-mdx-remote/rsc";
+
 export const metadata = {
   title: "Keith Bartholomew",
 };
 
-export default function Page() {
+export default async function Page() {
+  const posts = (await listPosts()).filter((p, idx) => {
+    if (idx <= 2) {
+      return p;
+    }
+    return null;
+  });
+
   return (
     <>
       <h1>Keith Bartholomew</h1>
@@ -30,6 +41,41 @@ export default function Page() {
         I’m an avid racing cyclist, and I enjoy cycling and cooking whenever I’m
         not at work.
       </p>
+
+      {posts.map((p) => (
+        <article key={p.data.slug} className="border mb-8">
+          <div className="md:flex justify-between border-b border-slate-100 p-4">
+            <div>
+              <Link href={`/blog/posts/${p.data.slug}`}>
+                <h3 className=" border-0 p-0 mb-0">{p.data.title}</h3>
+              </Link>
+            </div>
+            <div>
+              <time
+                className="whitespace-nowrap"
+                dateTime={p.data.date?.toISOString()}
+              >
+                {p.data.date?.toISOString()}
+              </time>
+            </div>
+          </div>
+          <div className="p-4">
+            {p.data.excerpt ? (
+              <MDXRemote source={p.data.excerpt} />
+            ) : (
+              <p>
+                <em>No excerpt</em>
+              </p>
+            )}
+            <Link
+              className="inline-block py-2 px-4 bg-blue-600 text-slate-100"
+              href={`/blog/posts/${p.data.slug}`}
+            >
+              Read
+            </Link>
+          </div>
+        </article>
+      ))}
     </>
   );
 }
